88430ed54fad8f0cee6df5b3356a078ff4cbd0eb
AnnA <anna_elfimova@mail.ru>
2022-08-09 13:48:00 +0300

    task 36 massiv odnomerniy sluchayniy show summa elementov nechetnix pozitsiy

---
+++ task36/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] array = new int[10];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 999);
+}
+
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (j % 2 == 1)
+    {
+     sum = sum + array[j];
+    } 
+}
+Console.WriteLine();
+Console.Write($"Сумма элементов, стоящих на нечётных позициях, = {sum}");
