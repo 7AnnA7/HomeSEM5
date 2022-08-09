d5d8b2cf09bedf4d21cba63f3b14df9cf566d492
AnnA <anna_elfimova@mail.ru>
2022-08-09 13:29:34 +0300

    task 34 massiv sluchaynix polozitelnix trexznachnix i pokazet kolichestvo chetnyx chisel v massive

---
+++ task34/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] array = new int[12];
+int countCH = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+for (int j = 0; j < array.Length; j++)
+{
+ if (array[j] % 2 == 0)
+ {
+  countCH = countCH + 1;
+ }
+}
+Console.Write($"Количество чётных чисел из массива: {countCH}");
